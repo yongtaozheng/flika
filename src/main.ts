292b8f835d5673f459286fe9965a36060615eba8
JYeontu <yongtaozheng0201@163.com>
2026-03-30 18:14:09 +0800

    入口埋点

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { createTracker } from '@jyeontu/web-tracker';
+
+const tracker = createTracker({
+  appId: 'flika',
+  reportUrl: 'http://localhost:3003/addVisitRecord',
+});
+
+// 登录后设置用户标识
+tracker.setUser('user_123');
 
 createApp(App).use(router).mount('#app')
